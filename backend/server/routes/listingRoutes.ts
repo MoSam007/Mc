6d3aba9d2d4f46bf41092ba5d17fe6471e159b26
MoSam007 <samato.moma@gmail.com>
2024-11-19 13:13:16 +0300

    Updated listing routes

--- backend/server/routes/listingRoutes.ts
+++ backend/server/routes/listingRoutes.ts
@@ -1,12 +1,12 @@
 import { Request } from 'express';
 import express from 'express';
-import { getAllListings, getListingById, createListing } from '../controllers/listingControllers';
+import { getAllListings, getListingById, createListing, updateListing, deleteListing} from '../controllers/listingControllers';
 import multer from 'multer';
 import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req: Request, file: any, cb: (arg0: null, arg1: string) => void) => {
-    cb(null, path.join(__dirname, '../../uploads')); // Adjusted path
+    cb(null, path.join(__dirname, '../../uploads')); 
   },
   filename: (_req: any, file: { originalname: any; }, cb: (arg0: null, arg1: string) => void) => {
     cb(null, `${Date.now()}-${file.originalname}`);
@@ -19,6 +19,8 @@ const router = express.Router();
 
 router.get('/', getAllListings);
 router.get('/:id', getListingById);
-router.post('/', upload.array('images', 5), createListing); 
+router.post('/', upload.array('images', 10), createListing);  
+router.put('/:id', upload.array('newImages', 10), updateListing); // Update listing route
+router.delete('/:id', deleteListing); 
 
 export default router;
